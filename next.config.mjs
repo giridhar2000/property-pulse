9222be80f410739650dfb1ee50a05126e2c46596
giridhar <giribala2001@gmail.com>
2024-04-15 02:04:01 +0530

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'lh3.googleusercontent.com',
-                pathname: '**'
-            }
-        ]
-    }
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "**",
+      },
+    ],
+  },
+  env: {
+    NEXT_PUBLIC_DOMAIN: "http://localhost:3000/",
+    NEXT_PUBLIC_API_DOMAIN: "http://localhost:3000/api",
+  },
 };
 
 export default nextConfig;
